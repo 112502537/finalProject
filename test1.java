2f1dc7ac013c20ec66c894c943c6d22278f21ed4
112502537 <a112502537@g.ncu.edu.tw>
2024-06-12 03:29:23 +0800

    Add files via upload

--- test1.java
+++ test1.java
@@ -12,21 +12,14 @@ public class test1 {
 
 	public static void main(String[] args) {
 		WChoose wc = new WChoose();//建立物件
-		
-		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		while(!s.equals("e")){
-			if(s.equals("1")) wc.enter("1");//進入地塊，傳入玩家名稱
-			else wc.enter("2");//進入地塊，傳入玩家名稱
-			wc.addWindowListener(new WindowAdapter() {
-				public void windowClosed(WindowEvent e) {
-					System.out.println(wc.getCost());//當視窗要關閉時回傳消耗值
-					wc.getContentPane().removeAll();//清空視窗
-					wc.removeWindowListener(this);//關閉追蹤
-				}
-			});
-			s = sc.next();
-		}
+		wc.enter("1");//進入地塊，傳入玩家名稱
+		wc.addWindowListener(new WindowAdapter() {
+			public void windowClosed(WindowEvent e) {
+				System.out.println(wc.getCost());//當視窗要關閉時回傳消耗值
+				wc.getContentPane().removeAll();//清空視窗
+				wc.removeWindowListener(this);//關閉追蹤
+			}
+		});
 	}
 
 }
